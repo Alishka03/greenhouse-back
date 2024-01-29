2bbc6f00118cf70bc8cd8659ca130a378ee8efc1
alisher <aytbayalisher@gmail.com>
2024-01-29 18:49:57 +0600

    adding file store

---
+++ file-store/src/main/java/kz/iitu/filestore/FileStoreApplication.java
@@ -0,0 +1,15 @@
+package kz.iitu.filestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class FileStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileStoreApplication.class, args);
+	}
+
+}
---
+++ file-store/src/test/java/kz/iitu/filestore/FileStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.iitu.filestore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
