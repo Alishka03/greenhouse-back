55a166421735d57944c72a91e59663802a1b82f5
alisher <alisheraytbay@gmail.com>
2024-02-26 18:00:18 +0600

    gateway deleted , added new greenhouse-gateway

---
+++ greenhouse-gateway/src/main/java/kz/diploma/greenhousegateway/GreenhouseGatewayApplication.java
@@ -0,0 +1,13 @@
+package kz.diploma.greenhousegateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenhouseGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenhouseGatewayApplication.class, args);
+	}
+
+}
---
+++ greenhouse-gateway/src/test/java/kz/diploma/greenhousegateway/GreenhouseGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.diploma.greenhousegateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenhouseGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
